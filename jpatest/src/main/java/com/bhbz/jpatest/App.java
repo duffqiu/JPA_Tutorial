6ab99f6b4032500ddef6c60f87f68cd47d40433c
egecqiu <duff.gechuan.qiu@ericsson.com>
2013-10-23 18:04:49 +0800

    using @IdClass to test compouse primary key

--- jpatest/src/main/java/com/bhbz/jpatest/App.java
+++ jpatest/src/main/java/com/bhbz/jpatest/App.java
@@ -16,80 +16,94 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 
-    /**
-     * @param args
-     *            macbook 2013年10月21日
-     */
-    public static void main(String[] args) {
-	ApplicationContext context = new ClassPathXmlApplicationContext(
-		"bean.xml");
+	/**
+	 * @param args
+	 *            macbook 2013年10月21日
+	 */
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"bean.xml");
 
-	/*	CustomerRepository customerRepository = context
-			.getBean(CustomerRepository.class);
+		/*
+		 * CustomerRepository customerRepository = context
+		 * .getBean(CustomerRepository.class);
+		 * 
+		 * customerRepository.save(new Customer("Duff-Romanty-Xiyue", 36,
+		 * "Guangzhou linhai Street 15-2-402"));
+		 * 
+		 * Iterable<Customer> customers = customerRepository
+		 * .findByUserNameLike("romanty"); for (Customer customer : customers) {
+		 * System.out.println(customer); }
+		 * 
+		 * Customer customer = customerRepository.findBySpecificId(9);
+		 * System.out.println(customer);
+		 * 
+		 * PollTopic pollTopic = new PollTopic("Poll topic3", new Date(
+		 * System.currentTimeMillis()), 1, 1, 1);
+		 * 
+		 * PollOptions pollOptions1 = new PollOptions("Item 3", 0); PollOptions
+		 * pollOptions2 = new PollOptions("Item 4", 0);
+		 * 
+		 * pollOptions1.setPollTopic(pollTopic);
+		 * pollOptions2.setPollTopic(pollTopic);
+		 * 
+		 * Set<PollOptions> pollOptionSet = new HashSet<>();
+		 * pollOptionSet.add(pollOptions1); pollOptionSet.add(pollOptions2);
+		 * 
+		 * pollTopic.setOptions(pollOptionSet);
+		 
 
-		customerRepository.save(new Customer("Duff-Romanty-Xiyue", 36,
-			"Guangzhou linhai Street 15-2-402"));
+		PollTopicRepository pollTopicRepository = context
+				.getBean(PollTopicRepository.class);
+		// pollTopic = pollTopicRepository.save(pollTopic);
 
-		Iterable<Customer> customers = customerRepository
-			.findByUserNameLike("romanty");
-		for (Customer customer : customers) {
-		    System.out.println(customer);
+		Iterable<PollTopic> pollTopics = pollTopicRepository.findAll();
+
+		for (PollTopic pTopic : pollTopics) {
+			System.out.println(pTopic);
 		}
 
-		Customer customer = customerRepository.findBySpecificId(9);
-		System.out.println(customer);
+		PollOptionsRepository pollOptionsRepository = context
+				.getBean(PollOptionsRepository.class);
+		PollOptions pollOptions = pollOptionsRepository.findOne(65);
 
-		PollTopic pollTopic = new PollTopic("Poll topic3", new Date(
-			System.currentTimeMillis()), 1, 1, 1);
+		if (pollOptions != null) {
+			PollTopic pollTopic = pollOptions.getPollTopic();
+			Set<PollOptions> pollOptionSet = pollTopic.getOptions();
 
-		PollOptions pollOptions1 = new PollOptions("Item 3", 0);
-		PollOptions pollOptions2 = new PollOptions("Item 4", 0);
+			Iterator<PollOptions> iterator = pollOptionSet.iterator();
 
-		pollOptions1.setPollTopic(pollTopic);
-		pollOptions2.setPollTopic(pollTopic);
+			while (iterator.hasNext()) {
+				PollOptions item = iterator.next();
+				System.out.println(item);
+			}
+		}
 
-		Set<PollOptions> pollOptionSet = new HashSet<>();
-		pollOptionSet.add(pollOptions1);
-		pollOptionSet.add(pollOptions2);
+		pollOptions.setOption_item("new item description");
 
-		pollTopic.setOptions(pollOptionSet);*/
+		pollOptionsRepository.save(pollOptions);
 
-	PollTopicRepository pollTopicRepository = context
-		.getBean(PollTopicRepository.class);
-	//	pollTopic = pollTopicRepository.save(pollTopic);
+		Iterable<PollTopic> topicIterator = pollTopicRepository
+				.findAllByIdGeaterThanOrderByTitleLowerDesc(0);
 
-	Iterable<PollTopic> pollTopics = pollTopicRepository.findAll();
+		for (PollTopic pTopic : topicIterator) {
+			System.out.println(pTopic);
+		}
+		*/
 
-	for (PollTopic pTopic : pollTopics) {
-	    System.out.println(pTopic);
+		CustomerRepository customerRepository = context
+				.getBean(CustomerRepository.class);
+
+		customerRepository.save(new Customer(1, "Duff-Romanty-Xiyue", 36,
+				"Guangzhou linhai Street 15-2-402", 1));
+		
+		customerRepository.save(new Customer(1, "Duff", 36,
+				"Guangzhou linhai Street 15-2-402", 2));
+
+		Iterable<Customer> customers = customerRepository
+				.findAllByIdAndType(1, 2);
+		for (Customer customer : customers) {
+			System.out.println(customer);
+		}
 	}
-
-	PollOptionsRepository pollOptionsRepository = context
-		.getBean(PollOptionsRepository.class);
-	PollOptions pollOptions = pollOptionsRepository.findOne(65);
-
-	if (pollOptions != null) {
-	    PollTopic pollTopic = pollOptions.getPollTopic();
-	    Set<PollOptions> pollOptionSet = pollTopic.getOptions();
-
-	    Iterator<PollOptions> iterator = pollOptionSet.iterator();
-
-	    while (iterator.hasNext()) {
-		PollOptions item = iterator.next();
-		System.out.println(item);
-	    }
-	}
-
-	pollOptions.setOption_item("new item description");
-
-	pollOptionsRepository.save(pollOptions);
-
-	Iterable<PollTopic> topicIterator = pollTopicRepository
-		.findAllByIdGeaterThanOrderByTitleLowerDesc(0);
-
-	for (PollTopic pTopic : topicIterator) {
-	    System.out.println(pTopic);
-	}
-
-    }
 }
--- jpatest/src/main/java/com/bhbz/jpatest/Customer.java
+++ jpatest/src/main/java/com/bhbz/jpatest/Customer.java
@@ -3,58 +3,116 @@
  */
 package com.bhbz.jpatest;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 /**
  * @author macbook
  * 
  *         2013年10月21日
  */
+
+class CustomerPk implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5246613685339770430L;
+
+	private int id;
+
+	private int type;
+	
+	public CustomerPk() {
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + type;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerPk other = (CustomerPk) obj;
+		if (id != other.id)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+
+}
+
+@IdClass(CustomerPk.class)
 @Entity(name = "CUSTOMER")
 public class Customer {
 
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
 
-    @Column(name = "USERNAME")
-    private String userName;
+	@Id
+	@Column(name = "ID")
+	private int id;
 
-    @Column(name = "AGE")
-    private int age;
+	@Id
+	@Column(name = "TYPE")
+	private int type;
 
-    @Column(name = "ADDRESS")
-    private String address;
+	@Column(name = "USERNAME")
+	private String userName;
 
-    protected Customer() {
+	@Column(name = "AGE")
+	private int age;
 
-    }
+	@Column(name = "ADDRESS")
+	private String address;
 
-    /**
-     * @param userName
-     * @param age
-     * @param address
-     */
-    public Customer(String userName, int age, String address) {
-	this.userName = userName;
-	this.age = age;
-	this.address = address;
-    }
+	protected Customer() {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-	return "Customer [id=" + id + ", userName=" + userName + ", age=" + age
-		+ ", address=" + address + "]";
-    }
+	}
+
+	/**
+	 * @param userName
+	 * @param age
+	 * @param address
+	 */
+	public Customer(int id, String userName, int age, String address, int type) {
+		this.id = id;
+		this.userName = userName;
+		this.age = age;
+		this.address = address;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", type=" + type + ", userName="
+				+ userName + ", age=" + age + ", address=" + address + "]";
+	}
+
+	
 
 }
