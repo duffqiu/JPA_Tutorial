7a3c584133b36352ccd17833617b789623714af5
Duff Qiu <duffqiu@qq.com>
2013-10-26 16:56:35 +0800

    Finish auto id for compose primary key

--- jpatest/src/main/java/com/bhbz/jpatest/Customer.java
+++ jpatest/src/main/java/com/bhbz/jpatest/Customer.java
@@ -7,8 +7,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
 
 /**
  * @author macbook
@@ -17,52 +21,51 @@ import javax.persistence.IdClass;
  */
 
 class CustomerPk implements Serializable {
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = -5246613685339770430L;
+    private static final long serialVersionUID = -5246613685339770430L;
 
-	private int id;
+    private int id;
 
-	private int type;
-	
-	public CustomerPk() {
+    private int type;
 
-	}
+    public CustomerPk() {
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + type;
-		return result;
-	}
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CustomerPk other = (CustomerPk) obj;
-		if (id != other.id)
-			return false;
-		if (type != other.type)
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	result = prime * result + type;
+	return result;
+    }
 
-	public int getType() {
-		return type;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	CustomerPk other = (CustomerPk) obj;
+	if (id != other.id)
+	    return false;
+	if (type != other.type)
+	    return false;
+	return true;
+    }
 
-	public void setType(int type) {
-		this.type = type;
-	}
+    public int getType() {
+	return type;
+    }
 
+    public void setType(int type) {
+	this.type = type;
+    }
 
 }
 
@@ -70,47 +73,49 @@ class CustomerPk implements Serializable {
 @Entity(name = "CUSTOMER")
 public class Customer {
 
+    @Id
+    @Column(name = "ID")
+    @SequenceGenerator(name = "CustomerGenerator", sequenceName = "seq_customer", allocationSize = 20)
+    @GeneratedValue(generator = "CustomerGenerator", strategy = GenerationType.SEQUENCE)
+    private int id;
 
-	@Id
-	@Column(name = "ID")
-	private int id;
+    @Id
+    @Column(name = "TYPE")
+    private int type;
 
-	@Id
-	@Column(name = "TYPE")
-	private int type;
+    @Column(name = "USERNAME")
+    private String userName;
 
-	@Column(name = "USERNAME")
-	private String userName;
+    @Column(name = "AGE")
+    private int age;
 
-	@Column(name = "AGE")
-	private int age;
+    @Column(name = "ADDRESS")
+    private String address;
 
-	@Column(name = "ADDRESS")
-	private String address;
+    @Version
+    private long version;
 
-	protected Customer() {
+    protected Customer() {
 
-	}
+    }
 
-	/**
-	 * @param userName
-	 * @param age
-	 * @param address
-	 */
-	public Customer(int id, String userName, int age, String address, int type) {
-		this.id = id;
-		this.userName = userName;
-		this.age = age;
-		this.address = address;
-		this.type = type;
-	}
+    /**
+     * @param userName
+     * @param age
+     * @param address
+     */
+    public Customer(String userName, int age, String address, int type) {
+	//		this.id = id;
+	this.userName = userName;
+	this.age = age;
+	this.address = address;
+	this.type = type;
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", type=" + type + ", userName="
-				+ userName + ", age=" + age + ", address=" + address + "]";
-	}
-
-	
+    @Override
+    public String toString() {
+	return "Customer [id=" + id + ", type=" + type + ", userName="
+		+ userName + ", age=" + age + ", address=" + address + "]";
+    }
 
 }
